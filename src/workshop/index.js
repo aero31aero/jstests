57bf52595f072a8648df4cef04af03a02bf4798a
Rohitt Vashishtha <aero31aero@gmail.com>
2017-02-13 19:55:22 +0530

    Add Workshop Sample.

--- src/workshop/index.js
+++ src/workshop/index.js
@@ -1,1 +1,11 @@
- 
+ var myFunction = function() {
+     console.log("JS File Loaded.");
+     var list = document.getElementById("list");
+     var elements = list.getElementsByTagName("li");
+     console.log(elements);
+     for(i=0;i<elements.length;i++){
+     	var blue = 20*i;
+     	var color = "rgb(60,60," + blue + ")";
+     	elements[i].style.background = color; 
+     }
+ }
